51f4b033631e22d1a8728ae470fba132fd5dfa85
diverge_g <gareth@divergedigital.uk>
2020-07-04 23:18:05 +0100

    remove overflow hidden wrapper

--- src/Cols/index.js
+++ src/Cols/index.js
@@ -4,13 +4,13 @@ import { css } from 'styled-components/macro'
 // import defaults from '../defaults'
 
 const Cols = ({ children, vAlign, flip, space = 0, ...props }) => (
+  // <div
+  //   css={css`
+  //     overflow: hidden;
+  //   `}
+  // >
   <div
     css={css`
-      overflow: hidden;
-    `}
-  >
-    <div
-      css={css`
         min-width: 100%;
         box-sizing: border-box;
         display: flex;
@@ -38,11 +38,11 @@ const Cols = ({ children, vAlign, flip, space = 0, ...props }) => (
         `
         }
       `}
-      {...props}
-    >
-      {children}
-    </div>
+    {...props}
+  >
+    {children}
   </div>
+  // </div>
 )
 
 export default Cols
